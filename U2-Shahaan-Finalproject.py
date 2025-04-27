af910887393fa98a22dd27beec39cb48445c078d
Shahaan Abdulmajeed <shahaan.abdulmajeed@thekaustschool.org>
2025-04-27 12:13:52 +0300

    day 1 over
    now im starting core block

--- U2-Shahaan-Finalproject.py
+++ U2-Shahaan-Finalproject.py
@@ -1,9 +1,19 @@
-Print("Welcom to the Food waste Logger!")
-Print("Track how much food you waste each day for a week")
+print("Welcom to the Food waste Logger!")
+print("Track how much food you waste each day for a week")
 
 #List to store food waste amount
 food_waste = []
 
 #Loop for 7 days(a week)
 for day in range (1, 8):
-    waste = float(input(f"Day [day]: enter food waste in grams:")
+    waste = float(input(f"Day {day}: enter food waste in grams:"))
+    food_waste.append(waste)
+
+#calculations fo total and average
+total = sum(food_waste)                 # total food waste
+average = total/7                       #divided by 7 because its a week
+
+#output results ( not final)
+print(f"--- Weekly Summary ---")
+print(f"Total food waste: {total} grams")
+print(f"Average waste daily: {average:.1} grams")
