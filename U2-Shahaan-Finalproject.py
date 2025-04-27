3c2e351cad632eaafde34a656daa153e073b9639
Shahaan Abdulmajeed <shahaan.abdulmajeed@thekaustschool.org>
2025-04-27 13:05:35 +0300

    1:05

--- U2-Shahaan-Finalproject.py
+++ U2-Shahaan-Finalproject.py
@@ -6,14 +6,27 @@ food_waste = []
 
 #Loop for 7 days(a week)
 for day in range (1, 8):
+    #ask for waste input
     waste = float(input(f"Day {day}: enter food waste in grams:"))
+    # add whatever waste amount to list
     food_waste.append(waste)
 
-#calculations fo total and average
+#calculations fo total aaste.append(waste)nd average
 total = sum(food_waste)                 # total food waste
 average = total/7                       #divided by 7 because its a week
 
-#output results ( not final)
+#show weekly results
 print(f"--- Weekly Summary ---")
 print(f"Total food waste: {total} grams")
-print(f"Average waste daily: {average:.1} grams")
+print(f"Average waste daily: {round (average, 2)} grams")
+
+#give feedabck based on how much food wasted on average
+if average<50:
+    print("Amazing job! your wasting very less food, Keep this up!")
+elif average<100:
+    print("Good job, But theres still room to improve!")
+else:
+    print("Try reducing food waste next week!")
+
+#end of program
+print("\nThank you for using The Food Waste Logger! Come back next week!")
